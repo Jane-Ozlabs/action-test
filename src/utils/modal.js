0df0b38ffa0bcfcd2ccf90d5aa2994464e6b7ff5
Aaron <aaron@ozlabs.co.kr>
2022-11-25 12:10:43 +0900

    Fixed: 팝업 중첩 문제 변경

--- src/utils/modal.js
+++ src/utils/modal.js
@@ -1,6 +1,20 @@
-
 export function showModal({ self, key, data }) {
+  const keyArr = ["bonus-log-modal", "game-log-modal", "cash-log-modal", "tip-log-modal", "tip-modal"]  
+  for (let i = 0; i < keyArr.length; i++) {
+    if(keyArr[i] == key){
+      keyArr.splice(i, 1);
+      i--
+    }
+  }
+  console.log(keyArr)
+
+  for(let j=0; j<keyArr.length; j++){
+    console.log(keyArr[j])
+    self.$store.commit("NOMODAL", keyArr[j]);
+  }
+
   self.$store.commit("MODAL", { key, data: { isVisible: true, ...(data||{}) }, });
+
 }
 
 export function setModalResult({ self, key, data }) {
