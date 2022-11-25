dce4360685ecd896289c861c6583d2af0ca4cd4d
Jane-ozlabs <jane@ozlabs.co.kr>
2022-11-25 19:55:32 +0900

    feat : make main header component

---
+++ src/router/layouts/MainHeader.vue
@@ -0,0 +1,75 @@
+<template>
+  <header>
+    <img src="img/logo.png" class="logo" />
+    <div class="depth">
+      <span v-if="breadcrumb[0]">{{ breadcrumb[0].text }}</span>
+      <span v-if="breadcrumb[1]">{{ breadcrumb[1].text }}</span>
+      <span v-if="breadcrumb[2]">{{ breadcrumb[2].text }}</span>
+    </div>
+    <div class="profile" @click="showProfile" v-click-outside="hideProfile">
+      <img src="img/profile.png" />
+      <span class="name">{{ me && me.username }}</span>
+      <span class="logout-btn" :class="{ show: isClicked }" @click="logout">
+        Logout</span
+      >
+    </div>
+    <a href="javascript:;" class="menuBtn" @click="isClicked">
+      <img src="img/icon_menu.png" />
+    </a>
+  </header>
+</template>
+
+<script>
+import { logout } from '@/services/auth';
+import { mapState } from 'vuex';
+import { GetAuth } from '@/store';
+import Swal from 'sweetalert2';
+
+export default {
+  props: {
+    breadcrumb: {
+      default() {
+        return [];
+      },
+    },
+  },
+  data() {
+    return {
+      isClicked: false,
+    };
+  },
+  computed: {
+    ...mapState({
+      me() {
+        let myInfo = GetAuth();
+        return myInfo;
+      },
+    }),
+  },
+  methods: {
+    logout() {
+      Swal.fire({
+        title: 'Logout',
+        text: 'Are you sure you want to log out?',
+        showCancelButton: true,
+        confirmButtonColor: '#dc2f00',
+        confirmButtonText: `Yes, I'm sure`,
+        cancelButtonText: 'No, cancel it!',
+      }).then(result => {
+        if (result.isConfirmed) {
+          logout();
+          this.$router.push('/login');
+        } else {
+          return;
+        }
+      });
+    },
+    showProfile() {
+      this.isClicked = !this.isClicked;
+    },
+    hideProfile() {
+      this.isClicked = false;
+    },
+  },
+};
+</script>
